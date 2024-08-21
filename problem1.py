a350c439d2040b2f2a8fb2df3296ecd0c6485269
Schnretzl <wrhendley@gmail.com>
2024-08-21 14:09:01 -0500

    Add problem 1

---
+++ problem1.py
@@ -0,0 +1,13 @@
+# 1. Python List Transformation
+
+# Task 1: Sort the grades in descending order and print the sorted list.
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+grades.sort()
+grades.reverse()
+print(grades)
+
+
+# Task 2: Calculate the average grade and print it.
+
+average = sum(grades) / len(grades)
+print(average)
